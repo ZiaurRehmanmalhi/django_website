1242a29a9f9afe0c9f75970e9ffc7cb5913d3b32
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-05-05 23:37:15 +0500

    add_files

--- text_utils/views.py
+++ text_utils/views.py
@@ -1,26 +1,43 @@
+# http://127.0.0.1:8000/
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index2.html')
 
 
 def removepunc(request):
-    return HttpResponse("remove punc")
+    djtext = djtext = request.POST.get('text', 'default')
+    removepunc = request.POST.get('removepunc', 'off')
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+    return HttpResponse("removepunc")
 
 
-def capitalizefirst(request):
-    return HttpResponse("capitalize first")
+def tictac(request):
+    # return HttpResponse('our services')
+    return render(request, 'game.html')
 
 
-def newlineremove(request):
-    return HttpResponse("newlineremove")
+def portfolio(request):
+    return redirect('https://www.ziamalhi.com/')
 
 
 def spaceremove(request):
-    return HttpResponse("spaceremove")
+    return HttpResponse("space remove")
 
 
-def charcount(request):
-    return HttpResponse("charcount")
+def contact(request):
+    return HttpResponse("contact")
+
+
+def speach(request):
+    return render(request, 'text_to_speach.html')
